ba7791a65d7302b3b8b403573d74055c60457443
Your Name <you@example.com>
2021-09-11 00:52:13 +0100

    commit 3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css"
 export const Header =(props)=>{
     return(
         <header>
-        <img className="img-header"src="./images/header-x2.png"alt="headphones"/>
+        <img className="img-header"src="./images/header-x3.png"alt="headphones"/>
     <h1 className="title">Electronics</h1>
     </header>
     )
