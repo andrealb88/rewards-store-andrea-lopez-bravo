c5a3a6e82b27b703dde411ba87cdb1e5e05ba9b8
Your Name <you@example.com>
2021-09-12 00:48:05 +0100

    commit Nav ans App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useContext } from "react";
 import "./App.css";
 import { Header } from "./components/header/Header";
+import { Nav } from "./components/nav/Nav.jsx";
 // import { AppConotext } from "./context/AppConotext";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
     </div>
   );
 }
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css"
+
+export const Nav =(props) =>{
+    return(
+        <nav className="nav flex-row">
+        <div className="container-logo">
+        <img src="./images/aerolab-logo.svg" alt="logo" />
+            </div>
+            </nav>
+    )
+}
