8f12cc39a3443fa9215bbf20ac63a473d2170275
Your Name <you@example.com>
2021-09-13 11:26:53 +0100

    commit 12

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import React from "react";
+
+export const AppContext = React.createContext();
+
+
+export default function AppProvider({ children }) {
+    rerturn(
+        
+    )
+}
