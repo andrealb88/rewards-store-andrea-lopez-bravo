276a93a056d38aa97a54ef8c89c1ccc4c21cbfef
Your Name <you@example.com>
2021-10-05 22:41:04 +0100

    commit Processor

--- src/components/processor/Processor.jsx
+++ src/components/processor/Processor.jsx
@@ -1,4 +1,4 @@
-import "./Selector.css";
+import "./Processor.css";
 import React, { useContext, useEffect } from "react";
 import { AppContext } from "../../context/AppContext";
 import { addPoints } from "../../services/points";
