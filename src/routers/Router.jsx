8de11358ceb184f8458eb84a506145deb3294ddc
Your Name <you@example.com>
2021-12-17 11:31:40 +0000

    commit Route.jsx

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -3,15 +3,33 @@ import { Home } from "../pages/Home";
 import { History } from "../pages/History";
 import { Points } from "../pages/Points";
 import { NotFound } from "../components/notification/NotFound";
-import React from "react";
 
-export const Router  = () => {
+ 
+ export const Router  = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/history" element={<History/>}/>
       <Route path="points" element={<Points/>}/>
-      <Route path="*" element={<NotFound/>} />
+      <Route path="NotFound" element={<NotFound/>} />
     </Routes>
   );
 };
+ /* export const Router =()=>{
+   return (
+   <Switch>
+                <Route exact path="/acamica-rewards-store">
+               <Home />
+           </Route>
+          <Route path="/acamica-rewards-store/history">
+                 <History />
+           </Route>
+         <Route path="/acamica-rewards-store/points">
+              <Points />
+          </Route>
+           <Route path="*">
+               <NotFound />
+           </Route>
+       </Switch> 
+       )
+   } */
