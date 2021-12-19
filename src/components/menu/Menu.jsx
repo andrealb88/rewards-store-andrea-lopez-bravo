8248047f68e20be5640eb57f8b2df5e5903ad044
Your Name <you@example.com>
2021-12-19 21:35:39 +0000

    debugging routes

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 export const Menu =()=>{
     return(
         <ul className="menu flex-row">
-             <li className="pink-line"><Link className="normal-text" to="/acamica-rewards-store/">Home</Link></li>
-             <li className="pink-line"><Link className="normal-text" to="/acamica-rewards-store/points">Add points</Link></li>
-             <li className="pink-line"><Link className="normal-text" to="/acamica-rewards-store/history">History</Link></li>
+             <li className="pink-line"><Link className="normal-text" to="/rewards-store-andrea-lopez-bravo/">Home</Link></li>
+             <li className="pink-line"><Link className="normal-text" to="/rewards-store-andrea-lopez-bravo/points">Add points</Link></li>
+             <li className="pink-line"><Link className="normal-text" to="/rewards-store-andrea-lopez-bravo/history">History</Link></li>
          </ul>
     )
  }
--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,25 +1,20 @@
-import { Router,Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { History } from "../pages/History";
 import { Points } from "../pages/Points";
 import { NotFound } from "../components/notification/NotFound";
 
-
- 
- export const MyRouter  = () => {
+export const MyRouter = () => {
   return (
-    <Router>
-        <Routes>
-      <Route path="/history" element={<History/>}/>
-      <Route path="/points" element={<Points/>}/>
-      <Route path="/NotFound" element={<NotFound/>} />
-      <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="*" element={<Home />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/points" element={<Points />} />
+      <Route path="/NotFound" element={<NotFound />} />
     </Routes>
-    </Router>
-  
   );
 };
- /* export const Router =()=>{
+/* export const Router =()=>{
    return (
    <Switch>
                 <Route exact path="/acamica-rewards-store">
