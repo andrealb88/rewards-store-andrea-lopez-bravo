33d509a1bff7864e7a43fd872b8bc5d6d877a59f
Your Name <you@example.com>
2021-12-20 00:16:00 +0000

    commit fix bug Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,7 +2,7 @@ import React,{useContext} from "react";
 import "./Nav.css"
 import { AppContext } from "../../context/AppContext";
 import{Details} from "../detail/Detail";
-import { Menu } from "../menu/Menu";
+// import { Menu } from "../menu/Menu";
 
 export const Nav =() =>{
     const { user } = useContext(AppContext);
@@ -14,7 +14,7 @@ export const Nav =() =>{
             <div className="user-points flex-row-center">
                 <h3 className="username">{user.name}</h3>
                 <Details text={user.points} />
-                <Menu/>
+                {/* <Menu/> */}
             </div>
             </nav>
     )
