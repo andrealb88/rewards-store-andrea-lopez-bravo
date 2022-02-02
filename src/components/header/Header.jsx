ec5300de100319c01321af5c5fdb4e8feae66239
Your Name <you@example.com>
2022-02-02 22:31:57 +0000

    debugging Header.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export const Header =()=>{
     return(
         <header className="header">
         <img className="img-header"src="./images/header-x3.png"alt="headphones"/>
-    <h1 className="title">Electronics</h1>
+    <h1 className="principal-text">Electronics</h1>
     </header>
     )
 } 
